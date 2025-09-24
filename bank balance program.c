992525ddab525187cf3180f5ec91970775c7f772
lucy15019 <lucyrug2@gmail.com>
2025-09-24 12:16:22 +0300

    Add files via upload

---
+++ bank balance program.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+int age ;
+int ID_number ;
+int height ;
+int bank_balance ;
+int rent;
+
+    printf("Enter your age:\n");
+    scanf("%d",&age);
+    printf( "Your age is %d\n",age);
+    
+    printf("Enter your ID_number:\n");
+    scanf("%d",&ID_number);
+    printf("Your ID_number is %d\n",ID_number);
+    
+    printf("Enter your height:\n");
+    scanf("%d",&height);
+    printf(" Your height is %d\n", height);
+    
+    printf("Enter your bank_balance:\n");
+    scanf("%d",&bank_balance);
+    printf("Your bank_balance is %d\n", bank_balance);
+    
+    printf("Enter your rent:\n");
+    scanf("%d",&rent);
+    printf("Your rent is %d\n",rent);
+    
+    
+    return 0;
+}
