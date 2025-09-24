df1baa13c52f8cfde9ff61e69e0de947fe6361ab
lucy15019 <lucyrug2@gmail.com>
2025-09-24 12:17:31 +0300

    Add files via upload

---
+++ Displaying program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Simple program to display SE semester one, first year\n");
+    printf("1.Descrete Mathematics\n");
+    printf("2.Mathematucs for science \n");
+    printf("3.Fundamental of ICT\n");
+    printf("4.Computer System and Organisation\n");
+    printf("5. Communication Skills\n");
+    printf("6.Introduction to programming and problem solving\n");
+    printf("7.Fundamaentals of system analysis and design\n");
+    
+    return 0;
+}
